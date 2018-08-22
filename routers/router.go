99ac17006d5f27c3887182a165759deaec7d6c58
450792414 <450792414@qq.com>
2018-08-23 01:47:06 +0800

    add user area controller

--- routers/router.go
+++ routers/router.go
@@ -7,4 +7,18 @@ import (
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+	beego.Router("/api/v1.0/areas", &controllers.AreaController{},"get:GetArea")
+	beego.Router("/api/v1.0/houses/index", &controllers.HouseIndexController{},"get:GetHouseIndex")
+
+	//api/v1.0/session
+	beego.Router("/api/v1.0/session", &controllers.SessionController{},"get:GetSessionData;delete:DeleteSessionData")
+	//api/v1.0/sessions
+	//beego.Router("/api/v1.0/sessions", &controllers.SessionController{},"post:Login")
+	//api/v1.0/sessions
+	beego.Router("/api/v1.0/sessions", &controllers.SessionController{},"post:Login")
+
+	beego.Router("/api/v1.0/users", &controllers.UserController{},"post:Reg")
+	//api/v1.0/user/avatar
+	//beego.Router("/api/v1.0/user/avatar", &controllers.UserController{},"post:Postavatar")
+
 }
